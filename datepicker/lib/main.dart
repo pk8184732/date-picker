25f3eb47dbc9a17482d38afee534db3bf86ac08c
pujak <pk8184732@gmail.com>
2024-01-05 12:04:27 +0530

    project created

---
+++ datepicker/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController datecontroller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Padding(padding: EdgeInsets.all(30),
+        child: TextField(
+          controller: datecontroller,
+          decoration: InputDecoration(hintText: "DATE",
+            filled: true,prefixIcon: Icon(Icons.calendar_today,),enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue),),
+          ),readOnly: true,
+          onTap: () {
+            selectDate ();
+          },
+        ),
+
+      )),
+
+    );
+  }
+  Future<void> selectDate() async {
+    DateTime? picked=await showDatePicker(
+        context:context,
+        initialDate:DateTime.now(),
+        firstDate:DateTime(2000),
+        lastDate:DateTime(2100)
+    );
+    if(picked!=null){
+      setState(() {
+        datecontroller.text=picked.toString().split(" ")[0];
+      });
+    }
+  }
+}
